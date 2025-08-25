acb07c8ecaaa167ab81b5013187cc4c408d1da01
ndrw1221 <andrew891221@gmail.com>
2025-08-25 09:55:41 +0000

    Add config for training SAO-audio-prompt

---
+++ stable_audio_tools/configs/dataset_configs/audio_prompt/musiccoca_emb.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+def get_custom_metadata(info, audio):
+    latent_dir = Path(
+        "/home/ndrw1221/lab-projects/new-anime-dataset-preprocess/musiccoca_emb"
+    )
+    rel_path = Path(info["relpath"])
+    latent_path = latent_dir / rel_path.with_suffix(".npy")
+
+    return {"audio_prompt": str(latent_path)}
