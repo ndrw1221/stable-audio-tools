b671d4d083be27db03d6b72991692793864042b1
ndrw1221 <andrew891221@gmail.com>
2025-09-03 08:34:39 +0000

    Tidy up dataset and model configs

--- stable_audio_tools/configs/dataset_configs/custom_metadata/info_prompt.py
+++
@@ -1,7 +0,0 @@
-def get_custom_metadata(info, audio):
-
-    return {
-        "prompt": info["prompt"],
-        "seconds_start": info["seconds_start"],
-        "seconds_total": info["seconds_total"],
-    }
