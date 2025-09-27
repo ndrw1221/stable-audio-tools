b34a0b80383746418e76dc4e18d6408e31477b9d
ndrw1221 <andrew891221@gmail.com>
2025-09-27 08:03:07 +0000

    Use latest model in run_gradio.sh

--- run_gradio.sh
+++ run_gradio.sh
@@ -1,3 +1,3 @@
 python run_gradio.py \
---model-config stable_audio_tools/configs/model_configs/txt2audio/stable_audio_open_1_0_anime.json \
---ckpt-path unwrapped_ckpt/sao-anime-tags-full-step=200000.ckpt \
+--model-config stable_audio_tools/configs/model_configs/txt2audio/sao_anime/stable_audio_open_1_0_anime.json \
+--ckpt-path unwrapped_ckpt/sao-anime-new-step=660000.ckpt \
