8c4c52b3420d32faac513df487ef5b80a6ffdf02
ndrw1221 <andrew891221@gmail.com>
2025-10-03 18:53:15 +0800

    Update unwrap.sh

--- unwrap.sh
+++ unwrap.sh
@@ -1,4 +1,4 @@
 python unwrap_model.py \
---model-config stable_audio_tools/configs/model_configs/txt2audio/sao_pili/stable_audio_open_1_0_tags.json \
---ckpt-path 'output/sao-pili/tags-dit + vae_dec cont./checkpoints/epoch=155-step=100000.ckpt' \
---name sao-pili-tags-step=100000 \
+--model-config stable_audio_tools/configs/model_configs/txt2audio/sao_audio_prompt/stable_audio_open_1_0_audio_prompt+text.json \
+--ckpt-path output/sao-anime-audio-prompt/musiccoca-emb+text/checkpoints/epoch=3508-step=1000000.ckpt \
+--name sao-anime-audio-prompt+text-step=1000000 \
