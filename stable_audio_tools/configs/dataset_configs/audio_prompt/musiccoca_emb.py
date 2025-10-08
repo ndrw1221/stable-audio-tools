df5df7e8b9e4938d61f7454c8952610993b80129
ndrw1221 <andrew891221@gmail.com>
2025-10-08 17:27:59 +0800

    Update dataset paths in config files

--- stable_audio_tools/configs/dataset_configs/audio_prompt/musiccoca_emb.py
+++ stable_audio_tools/configs/dataset_configs/audio_prompt/musiccoca_emb.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def get_custom_metadata(info, audio):
     latent_dir = Path(
-        "/home/ndrw1221/lab-projects/new-anime-dataset-preprocess/musiccoca_emb"
+        "/mnt/gestalt/home/ndrw1221/lab-projects/new-anime-dataset-preprocess/musiccoca_emb"
     )
     rel_path = Path(info["relpath"])
     latent_path = latent_dir / rel_path.with_suffix(".npy")
--- stable_audio_tools/configs/dataset_configs/custom_metadata/anime_qwen_caption.py
+++ stable_audio_tools/configs/dataset_configs/custom_metadata/anime_qwen_caption.py
@@ -2,7 +2,7 @@ import json
 
 
 def get_custom_metadata(info, audio):
-    metadata_path = "/home/ndrw1221/lab-projects/anime-dataset-preprocess/anime-dataset/anime-dataset.json"
+    metadata_path = "/mnt/gestalt/home/ndrw1221/lab-projects/anime-dataset-preprocess/anime-dataset/anime-dataset.json"
     with open(metadata_path, "r") as f:
         metadata = json.load(f)
 
